9a52fddcb500ce3784291ac119b6a1db6c0d9633
Iraj masoudian <itachimasoudian@gmail.com>
2026-03-05 10:13:00 +0000

    Update memory allocator to support dynamic heap resizing and improve paging integration. Enhanced initialization sequence for better stability and performance in memory management.

--- os/src/kernel/multiboot.rs
+++ os/src/kernel/multiboot.rs
@@ -1,6 +1,14 @@
-use core::ffi::{c_char, CStr};
+use core::ffi::{c_char, c_void, CStr};
+use crate::consts::PAGE_FRAME_SIZE;
+use crate::kernel::paging::frames::{PhysAddr, FRAME_ALLOCATOR};
+
+unsafe extern "C" {
+    static ___KERNEL_DATA_START__: c_void; // Start address of OS image
+    static ___KERNEL_DATA_END__: c_void; // End address of OS image
+}
 
 #[repr(C, packed)]
+#[derive(Copy, Clone)]
 pub struct MultibootInfo {
     flags: u32,
     mem_lower: u32,
@@ -22,6 +30,7 @@ pub struct MultibootInfo {
 }
 
 #[repr(u32)]
+#[derive(Copy, Clone)]
 enum MultibootFlag {
     MemInfoAvailable = 1 << 0,
     BootDeviceInfoAvailable = 1 << 1,
@@ -38,6 +47,7 @@ enum MultibootFlag {
 }
 
 #[repr(C, packed)]
+#[derive(Copy, Clone)]
 struct MultibootSymbols {
     tabsize: u32,
     strsize: u32,
@@ -46,6 +56,7 @@ struct MultibootSymbols {
 }
 
 #[repr(C, packed)]
+#[derive(Copy, Clone)]
 struct VbeInfo {
     vbe_control_info: u32,
     vbe_mode_info: u32,
@@ -56,6 +67,7 @@ struct VbeInfo {
 }
 
 #[repr(C, packed)]
+#[derive(Copy, Clone)]
 pub struct FramebufferInfo {
     pub addr: u64,
     pub pitch: u32,
@@ -67,6 +79,7 @@ pub struct FramebufferInfo {
 }
 
 #[repr(C, packed)]
+#[derive(Copy, Clone)]
 pub union ColorInfo {
     rgb: ColorMask,
     palette: ColorPalette,
@@ -90,7 +103,25 @@ pub struct ColorMask {
     blue_mask_size: u8,
 }
 
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+struct MemoryMapEntry {
+    size: u32,
+    addr: u64,
+    len: u64,
+    typ: MemoryType
+}
+
+#[repr(u32)]
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum MemoryType {
+    Available = 1,
+    Reserved = 2,
+    Other
+}
+
 #[repr(u8)]
+#[derive(Copy, Clone)]
 pub enum FramebufferType {
     Indexed = 0,
     RGB = 1,
@@ -125,6 +156,65 @@ impl MultibootInfo {
             Some(&self.framebuffer_info)
         } else {
             None
+        }
+    }
+
+    pub fn init_phys_memory_allocator(&self) {
+        // End address of kernel image (must not be inserted into physical memory allocator!)
+        let kernel_end = unsafe { &___KERNEL_DATA_END__ as *const c_void as u64 };
+
+        // Access to the physical memory allocator
+        let mut allocator = FRAME_ALLOCATOR.lock();
+
+        if self.flags & (MultibootFlag::MemoryMapAvailable as u32) != 0 {
+            // Start address of the memory map
+            let mmap_addr = self.mmap_addr as *const u8;
+            // Offset into the memory map, pointing to the current entry
+            let mut offset = 0;
+
+            // Loop over each entry in the memory map
+            while offset < self.mmap_length {
+                // mmap_addr + offset = pointer to current entry
+                let entry_ptr = unsafe { mmap_addr.add(offset as usize) as *const MemoryMapEntry };
+                let entry = unsafe { &*entry_ptr };
+                let typ = entry.typ;
+
+                // entry.size is the size of the entry struct itself -> Adding it to offset lets offset point to the next entry
+                offset += size_of::<u32>() as u32 + entry.size;
+
+                // Check if the current entry describes an available block of physical memory
+                if typ != MemoryType::Available {
+                    continue;
+                }
+
+                let mut start = entry.addr;
+                let mut end = entry.addr + entry.len;
+
+                // Leave kernel memory free
+                if start < kernel_end {
+                    start = kernel_end;
+                    if start >= end {
+                        continue;
+                    }
+                }
+
+                // Align start and end address to 4096 byte
+                if start % (PAGE_FRAME_SIZE as u64) != 0 {
+                    start = (start / (PAGE_FRAME_SIZE as u64) + 1) * (PAGE_FRAME_SIZE as u64);
+                }
+                if end % (PAGE_FRAME_SIZE as u64) != 0 {
+                    end = (end / (PAGE_FRAME_SIZE as u64)) * (PAGE_FRAME_SIZE as u64);
+                }
+
+                // Insert block into physical memory allocator
+                let num_frames = ((end - start) / (PAGE_FRAME_SIZE as u64)) as usize;
+                if num_frames > 0 {
+                    kprintln!("Inserting physical memory block (Addr: 0x{:016x}, Size: {} frames)", start, num_frames);
+                    unsafe {
+                        allocator.free_block(PhysAddr::new(start), num_frames);
+                    }
+                }
+            }
         }
     }
 }
